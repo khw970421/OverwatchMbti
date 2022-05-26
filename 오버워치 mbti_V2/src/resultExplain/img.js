b88481ac12624d77ed714c1f2aeaa18d67e38baf
khw970421 <khw970421@kakao.com>
2022-05-26 12:51:04 +0900

    ✨ feat : 소전 영웅 추가
    바스티온 + 오리사 통합

--- 오버워치 mbti_V2/src/resultExplain/img.js
+++ 오버워치 mbti_V2/src/resultExplain/img.js
@@ -1,39 +1,41 @@
 let img = [
-  './img/hero/para.png',
-  './img/hero/orisa.png',
-  './img/hero/mei.png',
-  './img/hero/moira.png',
-  './img/hero/mac.png',
-  './img/hero/genzi.png',
-  './img/hero/zenyata.png',
-  './img/hero/sigma.png',
-  './img/hero/bri.png',
-  './img/hero/zarya.png',
-  './img/hero/mercy.png',
-  './img/hero/soldier.png',
-  './img/hero/dum.png',
-  './img/hero/diva.png',
-  './img/hero/tracer.png',
-  './img/hero/junkret.png'
-]
+  "./img/hero/para.png",
+  "./img/hero/orisa.png",
+  "./img/hero/mei.png",
+  "./img/hero/moira.png",
+  "./img/hero/mac.png",
+  "./img/hero/genzi.png",
+  "./img/hero/zenyata.png",
+  "./img/hero/sigma.png",
+  "./img/hero/bri.png",
+  "./img/hero/zarya.png",
+  "./img/hero/mercy.png",
+  "./img/hero/soldier.png",
+  "./img/hero/dum.png",
+  "./img/hero/diva.png",
+  "./img/hero/tracer.png",
+  "./img/hero/junkret.png",
+];
 
 let img2 = [
-  './img/hero/acho.png',
-  './img/hero/bastion.png',
-  './img/hero/ash.png',
-  './img/hero/simetra.png',
-  './img/hero/reaper.png',
-  './img/hero/hanzo.png',
-  './img/hero/ana.png',
-  './img/hero/winston.png',
-  './img/hero/torvion.png',
-  './img/hero/sombra.png',
-  './img/hero/batist.png',
-  './img/hero/widow.png',
-  './img/hero/rein.png',
-  './img/hero/ball.png',
-  './img/hero/lucio.png',
-  './img/hero/hog.png'
-]
+  "./img/hero/acho.png",
+  "./img/hero/bastion.png",
+  "./img/hero/ash.png",
+  "./img/hero/simetra.png",
+  "./img/hero/reaper.png",
+  "./img/hero/hanzo.png",
+  "./img/hero/ana.png",
+  "./img/hero/winston.png",
+  "./img/hero/torvion.png",
+  "./img/hero/sombra.png",
+  "./img/hero/batist.png",
+  "./img/hero/widow.png",
+  "./img/hero/rein.png",
+  "./img/hero/ball.png",
+  "./img/hero/lucio.png",
+  "./img/hero/hog.png",
+];
 
-export {img,img2}
+let img3 = ["./img/hero/sojourn.png"];
+
+export { img, img2, img3 };
--- 오버워치 mbti_V2/src/utils/function.js
+++ 오버워치 mbti_V2/src/utils/function.js
@@ -1,154 +1,162 @@
-import {q, questionTitle2, questionTitle3} from "../question/question.js";
-import {A, B} from "../question/selection/second1Answer.js";
-import {q3A, q3B} from "../question/selection/third2Answer.js";
-import {result, result2} from "../resultExplain/result.js";
+import { q, questionTitle2, questionTitle3 } from "../question/question.js";
+import { A, B } from "../question/selection/second1Answer.js";
+import { q3A, q3B } from "../question/selection/third2Answer.js";
+import { result, result2 } from "../resultExplain/result.js";
 
-let startTime,endTime;
+let startTime, endTime;
 var num = 1;
-let mbti = '';
+let mbti = "";
 let isMore;
 let timerId;
 
-const shuffleArray = array => {
-  for (let i = 1; i <= Object.keys(array).length;i++) {
-    let j = Math.floor(Math.random() * i+1);
+const shuffleArray = (array) => {
+  for (let i = 1; i <= Object.keys(array).length; i++) {
+    let j = Math.floor(Math.random() * i + 1);
     [array[i], array[j]] = [array[j], array[i]];
   }
 };
 
-
-function start(){
+function start() {
   startTime = new Date();
-  $('.start').hide();
-  $('.question').show();
+  $(".start").hide();
+  $(".question").show();
   shuffleArray(q);
   next();
 }
-$('#A').click(function(){
+
+$("#A").click(function () {
   clearTimeout(timerId);
 
-  if(num < 13) {
-    var type = $('#type').val();
-    var preValue = $('#' + type).val();
-    $('#' + type).val(parseInt(preValue) + 1);
-  }
-  else if (num ==13){
+  if (num < 13) {
+    var type = $("#type").val();
+    var preValue = $("#" + type).val();
+    $("#" + type).val(parseInt(preValue) + 1);
+  } else if (num == 14) {
     isMore = true;
   }
+  console.log("Aclick", isMore, num);
   next();
 });
-$('#B').click(function(){
+
+$("#B").click(function () {
   clearTimeout(timerId);
 
-  if (num ==13){
+  if (num == 14) {
     isMore = false;
   }
   next();
 });
 
-function next(){
-  if(num == 13){
+function next() {
+  if (num == 13) {
     ganimedes();
-    ($("#EI").val() >= 2) ? mbti += "E" : mbti += "I";
-    ($("#SN").val() >= 2) ? mbti += "S" : mbti += "N";
-    ($("#TF").val() >= 2) ? mbti += "T" : mbti += "F";
-    ($("#JP").val() >= 2) ? mbti += "J" : mbti += "P";
+    $("#EI").val() >= 2 ? (mbti += "E") : (mbti += "I");
+    $("#SN").val() >= 2 ? (mbti += "S") : (mbti += "N");
+    $("#TF").val() >= 2 ? (mbti += "T") : (mbti += "F");
+    $("#JP").val() >= 2 ? (mbti += "J") : (mbti += "P");
     num++;
-    $('#title').html(questionTitle2[mbti]);
+    $("#title").html(questionTitle2[mbti]);
 
-    $('#A').html(A[mbti]);
-    $('#B').html(B[mbti]);
-
-  }
-  else if(num < 13) {
+    $("#A").html(A[mbti]);
+    $("#B").html(B[mbti]);
+  } else if (num < 13) {
     ganimedes();
-    $('.progress-bar').attr('style', 'width: calc(100/16*' + num + '%)');
-    $('#title').html(q[num]["title"]);
-    $('#type').val(q[num]["type"]);
-    $('#A').html(q[num]["A"]);
-    $('#B').html(q[num]["B"]);
+    $(".progress-bar").attr("style", "width: calc(100/16*" + num + "%)");
+    $("#title").html(q[num]["title"]);
+    $("#type").val(q[num]["type"]);
+    $("#A").html(q[num]["A"]);
+    $("#B").html(q[num]["B"]);
 
     num++;
-    $('#A').click(function(){
+    $("#A").click(function () {
       return false;
     });
-    $('#B').click(function(){
+    $("#B").click(function () {
       return false;
     });
-  }
-  else if(num == 14){
+  } else if (num == 14) {
     ganimedes();
-    $('.progress-bar').attr('style', 'width: calc(100/16*' + num + '%)');
-    $('#title').html(questionTitle3['1']);
+    $(".progress-bar").attr("style", "width: calc(100/16*" + num + "%)");
+    $("#title").html(questionTitle3["1"]);
 
-    $('#A').html(q3A['1']);
-    $('#B').html(q3B['1']);
+    $("#A").html(q3A["1"]);
+    $("#B").html(q3B["1"]);
 
     num++;
-    $('#A').click(function(){
+    $("#A").click(function () {
       return false;
     });
-    $('#B').click(function(){
+    $("#B").click(function () {
       return false;
     });
-  }
-  else if(num == 15){
+  } else if (num == 15) {
     ganimedes();
-    $('.progress-bar').attr('style', 'width: calc(100/16*' + num + '%)');
-    $('#title').html(questionTitle3['2']);
+    $(".progress-bar").attr("style", "width: calc(100/16*" + num + "%)");
+    $("#title").html(questionTitle3["2"]);
 
-    $('#A').html(q3A['2']);
-    $('#B').html(q3B['2']);
+    $("#A").html(q3A["2"]);
+    $("#B").html(q3B["2"]);
 
     num++;
-    $('#A').click(function(){
+    $("#A").click(function () {
       return false;
     });
-    $('#B').click(function(){
+    $("#B").click(function () {
       return false;
     });
-  }
-  else {
+  } else {
     endTime = new Date();
-    if ((endTime.getTime() - startTime.getTime()) / (1000) < 20) {
-      $('.question').hide();
-      $('.kakao_ad').hide();
-      $('body').css('background-image', 'url("")');
-      document.querySelector('.video').style.display = 'block';
-      document.querySelector('.videoTitle0').style.display = 'block'
-    }
-    else {
-      $('.question').hide();
-      $('.result').show();
+    if ((endTime.getTime() - startTime.getTime()) / 1000 < 20) {
+      $(".question").hide();
+      $(".kakao_ad").hide();
+      $("body").css("background-image", 'url("")');
+      document.querySelector(".video").style.display = "block";
+      document.querySelector(".videoTitle0").style.display = "block";
+    } else {
+      $(".question").hide();
+      $(".result").show();
+
+      console.log(isMore, result2[mbti]["HERO"]);
+
       if (isMore) {
-        $('#img').attr('src', result[mbti]["img"]);
-        $('#HERO').html(result[mbti]["HERO"]);
-        $('#explain').html(result[mbti]["explain"]);
-        $('#name').html(result[mbti]["HERO"] + '의 대사');
+        $("#img").attr("src", result[mbti]["img"]);
+        $("#HERO").html(result[mbti]["HERO"]);
+        $("#explain").html(result[mbti]["explain"]);
+        $("#name").html(result[mbti]["HERO"] + "의 대사");
         for (let i = 0; i < result[mbti]["line"].length; i++) {
-          document.getElementById('line').insertAdjacentHTML('afterend', `<h3 class="text-center mt-1 skyblueText properSize">${result[mbti]["line"][i]}</h3>`);
+          document
+            .getElementById("line")
+            .insertAdjacentHTML(
+              "afterend",
+              `<h3 class="text-center mt-1 skyblueText properSize">${result[mbti]["line"][i]}</h3>`
+            );
         }
       } else {
-        $('#img').attr('src', result2[mbti]["img"]);
-        $('#HERO').html(result2[mbti]["HERO"]);
-        $('#explain').html(result2[mbti]["explain"]);
-        $('#name').html(result2[mbti]["HERO"] + '의 대사');
+        $("#img").attr("src", result2[mbti]["img"]);
+        $("#HERO").html(result2[mbti]["HERO"]);
+        $("#explain").html(result2[mbti]["explain"]);
+        $("#name").html(result2[mbti]["HERO"] + "의 대사");
         for (let i = 0; i < result2[mbti]["line"].length; i++) {
-          document.getElementById('line').insertAdjacentHTML('afterend', `<h3 class="text-center mt-1 skyblueText properSize">${result2[mbti]["line"][i]}</h3>`);
+          document
+            .getElementById("line")
+            .insertAdjacentHTML(
+              "afterend",
+              `<h3 class="text-center mt-1 skyblueText properSize">${result2[mbti]["line"][i]}</h3>`
+            );
         }
       }
     }
   }
 }
 
-function ganimedes(){
-  timerId = setTimeout(() =>{
-    $('.question').hide();
-    $('.kakao_ad').hide();
-    $('body').css('background-image', 'url("")');
-    document.querySelector('.video1').style.display = 'block';
-    document.querySelector('.videoTitle1').style.display = 'block';
+function ganimedes() {
+  timerId = setTimeout(() => {
+    $(".question").hide();
+    $(".kakao_ad").hide();
+    $("body").css("background-image", 'url("")');
+    document.querySelector(".video1").style.display = "block";
+    document.querySelector(".videoTitle1").style.display = "block";
   }, 60000);
 }
 
-export {start}
+export { start };
